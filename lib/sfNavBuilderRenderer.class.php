e6f4d95a0df1514e6c77a88bc0b87d42e9d4a4be
Roberto Bermejo Martinez <roberto@robertobermejo.es>
2012-07-09 15:01:01 +0200

    Add option to allow rendering of the menu

--- lib/sfNavBuilderRenderer.class.php
+++ lib/sfNavBuilderRenderer.class.php
@@ -12,20 +12,16 @@ class sfNavBuilderRenderer implements sfNavBuilderRendererInterface
     public function render(sfNavBuilder $menu)
     {
         $ret = '';
+        if (method_exists($menu, 'render'))
+        {
+            $ret .= $item->render();
+        } else
+        {
         foreach ($menu->getItems() as $item)
         {
-            $ret .= '<li><a class="' . ($item->isActive() ? 'selected' : '') . '" href="' . $item->getUrl() . '">' . $item . '</a></li>';
-
-            if ($item->hasChildren() && $item->isActive())
-            {
-                $ret .= '<li><ul id="subnavlist">';
-                foreach ($item->getChildren() as $child)
-                {
-                    $ret .= '<li><a class="' . ($child->isActive() ? 'subSelected' : '') . '" href="' . $child->getUrl() . '">' . $child . '</a></li>';
-                }
-                $ret .= '</ul></li>';
-            }
+            $ret .= $this->renderItem($item);
         }
+	}
         return $ret;
     }
 
